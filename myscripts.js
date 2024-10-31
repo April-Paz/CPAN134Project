ab4a7a79ee6a39ec2bdfe8e7755d6cb25dda50e8
April-Paz <april.m.paz@gmail.com>
2024-10-31 10:02:58 -0400

    clean up

--- myscripts.js
+++ myscripts.js
@@ -4,9 +4,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction(){
     if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
         document.getElementById("navigation").style.padding = "3px";
+        document.getElementById("navigation").style.fontSize = "15px";
     }else{
-        document.getElementById("navigation").style.padding = "12px";
+        document.getElementById("navigation").style.padding = "10px";
+        document.getElementById("navigation").style.fontSize = "16px";
     }
 }
-
-//Photography Page
